3f73273e82406cff46f16b10b6a998ca5b2e3257
sats0k <gsats0k@gmail.com>
2026-02-11 14:33:35 +0200

    Fix encrypted wallet keys restoring as uncompressed

--- src/key.cpp
+++ src/key.cpp
@@ -214,7 +214,7 @@ bool CKey::SetPrivKey(const CPrivKey& vchPrivKey) {
 
     if (vchPrivKey.size() == 32) {
         CSecret secret(vchPrivKey.begin(), vchPrivKey.end());
-        return SetSecret(secret, false);
+        return SetSecret(secret, true);
     }
 
     const unsigned char* p = vchPrivKey.data();
@@ -338,10 +338,6 @@ bool CKey::SetPubKey(const CPubKey& vchPubKeyIn) {
 }
 
 CPubKey CKey::GetPubKey() const {
-    if (!fCompressedPubKey) {
-        // legacy key → force upgrade
-        const_cast<CKey*>(this)->fCompressedPubKey = true;
-    }
     return vchPubKey;
 }
 
