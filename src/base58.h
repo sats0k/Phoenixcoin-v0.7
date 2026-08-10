d6f01f67ee2e0c1f1439c7fde487c656899fde32
sats0k <gsats0k@gmail.com>
2026-08-10 21:57:06 +0300

    Increase compiler optimization level from O2 to O3
    Remove -Wno-ignored-qualifiers

--- src/base58.h
+++ src/base58.h
@@ -210,9 +210,8 @@ public:
     }
 
     std::string ToString() const {
-        std::vector<uchar> vch;
-        vch.resize(1, nVersion);
-        vch.swap(vch);
+        std::vector<unsigned char> vch(1);
+        vch[0] = nVersion;
         vch.insert(vch.end(), vchData.begin(), vchData.end());
         return EncodeBase58Check(vch);
     }
